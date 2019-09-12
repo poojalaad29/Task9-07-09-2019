b9305192e39024a22f4e1232297e0108e8e2ba42
Pooja laad <Pooja laad@LAPTOP-ER3OQRDU>
2019-09-12 10:32:23 +0530

    Task date 11-09-2019

--- src/com/wp/BookServlet.java
+++ src/com/wp/BookServlet.java
@@ -22,42 +22,38 @@ public class BookServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     
 		String subject[]=request.getParameterValues("Subject");
-		System.out.println(subject.length);
-		for(int i=0; i<subject.length; i++){
-			System.out.println(subject[i].toString());
-	   }
-
+		
 		for(int i=0;i<subject.length;i++)
             {
-			s.add(i, subject[i].split("/"));
-			}
-		
+			String v=subject[i].replace("/","");
+			s.add(i, v);
+		   
+            }
+		 System.out.println(s);
 		PrintWriter out=response.getWriter();
-			
-		Cookie c3 = new Cookie("visit", subject[1]);
-		response.addCookie(c3);
 		
 		try{
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","root");
-		
-		String sql="SELECT id,Title from books";
+		for(int i=0;i<subject.length;i++)
+        {
+		String sql="SELECT id,Title from books where Subject='"+s.get(i)+"'";
+		System.out.println(sql);
 		PreparedStatement ps=con.prepareStatement(sql);
-		//ps.setString(1, subject);
 		ResultSet rs=ps.executeQuery();
 		
-		out.println("<html>");
 		out.println("<html><body>");
-		out.println("<h3>Select The Desired Title</h3>");
+		out.println("<h3>Select The Desired Title of "+s.get(i)+"</h3>");
 		out.println("<hr>");
 		
 		while(rs.next()){
 			String code=rs.getString(1);
 			String title=rs.getString(2);
-			
+		
 			out.println("<a href=BookDetailServlet?code="+code+"&title="+title+">");
 			out.println(title);
 			out.println("</a><br>");
+		 }
 		}
 		out.println("<hr>");
 		out.println("<a href=SubjectPageServlet>Subject-Page</a>");
